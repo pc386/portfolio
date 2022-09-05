fafa28a8a4d5a988c5e8ea1ac179d733f7ea5044
Petar <petar@cekic.xyz>
2022-09-05 16:45:27 +0200

    temporarly remove the blog link as it's a WIP

--- src/App.js
+++ src/App.js
@@ -29,9 +29,9 @@ class App extends React.Component {
       <Container className="App">
         <Welcome help={help} />
         {this.terminal}
-        <a href="//blog.cekic.xyz" className="blog-link">
+        {/* <a href="//blog.cekic.xyz" className="blog-link">
           Go to Blog
-        </a>
+        </a> */}
       </Container>
     );
   }
